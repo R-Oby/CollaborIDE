d760b81761b27e4aae71cb683527b6aad0bce416
RobertasBr <s00238718@atu.ie>
2025-11-05 12:33:18 +0000

    Spring Initalizr

---
+++ src/main/java/com/r_oby/collaborIDE/CollaborIdeApplication.java
@@ -0,0 +1,13 @@
+package com.r_oby.collaborIDE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborIdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborIdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/r_oby/collaborIDE/CollaborIdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.r_oby.collaborIDE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborIdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
